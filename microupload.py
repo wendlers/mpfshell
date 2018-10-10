9d11e9c39a0289534584531032dfce8e33ffa2c6
Juwan <32978053+junhuanchen@users.noreply.github.com>
2018-10-10 17:43:57 +0800

    fix microupload, pycharm run no support.

--- microupload.py
+++ microupload.py
@@ -9,7 +9,7 @@ def main(args: List[str]) -> None:
 
     from mp.mpfshell import main
 
-    sys.argv = [args[0], '-c', 'open', args[4] + ';', 'lcd', args[2][args[2].find('/'):] + ';', 'lef', os.path.basename(args[5]) + ';', '--nocolor']
+    sys.argv = [args[0], '-c', 'open', args[4] + ';', 'lcd', args[2][args[2].find('/'):] + ';', 'runfile', os.path.basename(args[5]) + ';', '-n']
     main()
 
 if __name__ == '__main__':
