1b9d87c41e8ae8acd7207493e6ed1bc2699352a1
Stavros Korokithakis <hi@stavros.io>
2018-12-02 23:31:55 +0200

    Fix conflict.

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-from mp import version
 from setuptools import setup
 
+from mp import version
 
-setup(name='mpfshell',
-      version=version.FULL,
-      description='A simple shell based file explorer for ESP8266 and WiPy '
-                  'Micropython devices.',
-      author='Stefan Wendler',
-      author_email='sw@kaltpost.de',
-      url='https://github.com/wendlers/mpfshell',
-      download_url='https://github.com/wendlers/mpfshell/archive/0.8.1.tar.gz',
-      install_requires=['pyserial', 'colorama', 'websocket_client'],
-      packages=['mp'],
-      keywords=['micropython', 'shell', 'file transfer', 'development'],
-      classifiers=[],
-      entry_points={
-        'console_scripts': [
-            'mpfshell=mp.mpfshell:main',
-        ]
-      },
-      )
+setup(
+    name="mpfshell",
+    version=version.FULL,
+    description="A simple shell based file explorer for ESP8266 and WiPy "
+    "Micropython devices.",
+    author="Stefan Wendler",
+    author_email="sw@kaltpost.de",
+    url="https://github.com/wendlers/mpfshell",
+    download_url="https://github.com/wendlers/mpfshell/archive/0.8.1.tar.gz",
+    install_requires=["pyserial", "colorama", "websocket_client"],
+    packages=["mp"],
+    scripts=["mpfshell"],
+    keywords=["micropython", "shell", "file transfer", "development"],
+    classifiers=[],
+    entry_points={"console_scripts": ["mpfshell=mp.mpfshell:main"]},
+)
